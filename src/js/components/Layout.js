b6e4c57123ebb304ea3341955d0a8cd3c55daefa
Hideto Yasunori <hideto0118yasunori@gmail.com>
2019-12-22 12:31:54 -0800

    Add state

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -7,6 +7,7 @@ export default class Layout extends React.Component {
   constructor() {
     super();
     this.petName = "Buchi🐶";
+    this.state = { name: "Eddie" };
   }
   
   render() {
@@ -14,14 +15,18 @@ export default class Layout extends React.Component {
     let name = "Hideto";
     let components = [<Header />, <Footer />];
     
+    setTimeout(
+      () => { this.setState({name: "Hello"}); }
+      , 1000);
+
     return (
       //can you write variables here?
-
       <div>
         <Header />
         <Header />
         <hr />
         <div>
+          <h1>This is state name {this.state.name}</h1>
           <h1>Hi it's {name}!</h1>
           <h1>My cuttie doggy is {this.petName}!</h1>
           <h2>I'm {this.getAge(20, 9)} years old</h2>
